2d2be5ecfd7af08bd9083d9cd082592cf8a5e0b9
Dmitri <dima337dk@gmail.com>
2025-12-02 18:18:34 +0200

    TL-22-Homework

--- tests/ui/e2e-with-mocks.spec.ts
+++ tests/ui/e2e-with-mocks.spec.ts
@@ -3,9 +3,10 @@ import { LoginPage } from '../pages/login-page'
 import { OrderPage } from '../pages/order-page'
 import FoundPage from '../pages/found-page'
 import { SERVICE_URL } from '../../config/env-data'
+import NotFoundPage from '../pages/not-found-page'
 
 const jwt =
-  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmRyZWlrcm0iLCJleHAiOjE3NjQ2MjY5NDMsImlhdCI6MTc2NDYwODk0M30.qY5NsWSqtybtpPCfwFtvwU66BHEupsapz8o_vONaB3s0POp26lvbaKdhYLeO3m_nrNePiYhmCGl2z0PWgZ228w'
+  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkbWl0cmlraXJsaSIsImV4cCI6MTc2NDcwMDcwNiwiaWF0IjoxNzY0NjgyNzA2fQ.vX4LGmFZ9cG9yEzT9t4671LvSIchPHu4KxisRh5_ySe8dpAh08gUc4v7dGiBQCIQaHqT2pNAwLrhq6OgEvUyOg'
 
 test('TL-22-1 signIn with mocks', async ({ page }) => {
   const loginPage = new LoginPage(page)
@@ -40,6 +41,8 @@ test('TL-22-2 create and find order with mocks', async ({ context }) => {
   // await loginPage.passwordField.fill('test1234'); <--- disabled because of JWT
   // await loginPage.signInButton.click(); <--- disabled because of JWT
 
+  await expect(orderPage.phoneField).toBeVisible()
+
   await orderPage.nameField.fill(newOrder.customerName)
   await orderPage.phoneField.fill(newOrder.customerPhone)
   await orderPage.commentField.fill(newOrder.comment)
@@ -68,4 +71,41 @@ test('TL-22-2 create and find order with mocks', async ({ context }) => {
   await orderPage.trackButton.click()
   await trackOrderResponse
   expect(await foundPage.orderName.innerText()).toBe(newOrder.customerName)
+})
+
+test('TL-22-3 create and not find order with mocks', async ({ context }) => {
+  await context.addInitScript((token) => {
+    localStorage.setItem('jwt', token)
+  }, jwt)
+  const page = await context.newPage()
+  const loginPage = new LoginPage(page)
+  const orderPage = new OrderPage(page)
+  const notFoundPage = new NotFoundPage(page)
+  await loginPage.open()
+  await orderPage.statusButton.click()
+  await orderPage.fillElement(orderPage.orderIdInputField, String(-1))
+  await orderPage.trackButton.click()
+  await expect(notFoundPage.title).toBeVisible()
+})
+
+test('TL-22-4 code 500', async ({ context }) => {
+  await context.addInitScript((token) => {
+    localStorage.setItem('jwt', token)
+  }, jwt)
+  const page = await context.newPage()
+  const loginPage = new LoginPage(page)
+  const orderPage = new OrderPage(page)
+  await loginPage.open()
+  await orderPage.statusButton.click()
+  await orderPage.fillElement(orderPage.orderIdInputField, String(-1))
+
+  await page.route('**/orders/*', async (route) => {
+    await route.fulfill({
+      status: 500,
+    })
+  })
+  const trackOrderResponse = page.waitForResponse('**/orders/*')
+  await orderPage.trackButton.click()
+  const response = await trackOrderResponse
+  expect(response.status()).toBe(500)
 })
